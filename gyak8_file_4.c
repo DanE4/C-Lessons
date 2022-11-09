9936398dcf655f0938695d2fa8e3364b8a66ef98
DanE4 <erosdani53@gmail.com>
2022-11-09 19:04:29 +0100

    .

--- gyak8_file_4.c
+++ gyak8_file_4.c
@@ -7,11 +7,18 @@ int main(){
     int result;
     do{
         fscanf(f1,"%d",&tmp1);
+        fgetc(f1);
         fscanf(f2,"%d",&tmp2);
+        fgetc(f2);
+        printf("%d \n",tmp1*tmp2);
     }
-    while(result != EOF);
+    while(result != 0);
     fclose(f1);
     fclose(f2);
-    
+    //scanf(format,...)stdről olvas adott formatumu dolgokat
+    //fscanf(file,format,...)file-ból olvas adott formatumu dolgokat
+    //fgets(file,format,...)beolvas és visszaadt EGY karaktert egy filebol
+    //pl: char a=fgetc(stdin);
+    //fgets(dest,size,file)
     return 1;
 }
